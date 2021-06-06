f7dba69f0112870117d816c52afb6fbd66c400a7
tebaly <support@freedomsex.net>
2021-06-06 12:02:46 +0300

    init

---
+++ components/Test.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: ["route"],
+  data: () => ({
+    needed: false
+  }),
+  components: {},
+  mounted() {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<template>
+  <div>
+    Test Hello
+  </div>
+</template>
+
+<style lang="scss"></style>
